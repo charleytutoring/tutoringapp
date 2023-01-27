baaca097542d5f87cf5b4800c83f28374fc8db9e
charleytutoring <charley@tutoring.co.kr>
2023-01-27 12:16:26 +0900

    [real] /feat react-query 초기 세팅

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+"use client";
+
+import { useGetTopic } from "@/core/api/topic/queries";
+import Image from "next/image";
+
 export default function Home() {
+  const { data: Topic, isLoading } = useGetTopic();
+
+  if (isLoading) {
+    return <div>loading...</div>;
+  }
   return (
     <main>
-      <span className="text-red-500">hi</span>
+      {Topic &&
+        Topic.map((card) => {
+          return (
+            <div key={card.id} className="flex">
+              <span>{card.name}</span>
+              <Image width={100} height={100} alt={card.name} src={card.avatar}/>
+            </div>
+          );
+        })}
     </main>
-  )
+  );
 }
